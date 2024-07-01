aaee2257ee4f21638a6f352aeef50da7eca0e03f
Themeintro <wpdvapu@gmail.com>
2024-07-01 17:22:15 +0600

    update_nameing_conversion

--- includes/admin-menu.php
+++ includes/admin-menu.php
@@ -8,52 +8,52 @@ class Post_Management_Admin_Menu{
     }
 
     public function admin_menu () {
-        add_menu_page( 'Post Management', 'Post Management', 'manage_options', 'post-managements', array($this,'custom_post_menu'), 'dashicons-chart-pie', 2 );
+        add_menu_page( 'Post Management', 'Post Management', 'manage_options', 'post-managements', array($this,'post_management_menu'), 'dashicons-chart-pie', 2 );
     }
 
-    public function custom_post_menu () {
+    public function post_management_menu () {
 
-    // Check if a category ID is set and get its value
-    if ( isset ( $_GET[ 'category-id' ] ) ) {
-        $selected_category_id = intval( $_GET [ 'category-id' ] );
-    } else {
-        $selected_category_id = -1;
-    }
+        // Check if a category ID is set and get its value
+        if ( isset ( $_GET[ 'category-id' ] ) ) {
+            $selected_category_id = intval( $_GET [ 'category-id' ] );
+        } else {
+            $selected_category_id = -1;
+        }
 
-    // Check if an author ID is set and get its value
-    if ( isset ( $_GET [ 'author-id' ] ) ) {
-        $selected_author_id = intval ( $_GET[ 'author-id' ] );
-    } else {
-        $selected_author_id = -1;
-    }
+        // Check if an author ID is set and get its value
+        if ( isset ( $_GET [ 'author-id' ] ) ) {
+            $selected_author_id = intval ( $_GET[ 'author-id' ] );
+        } else {
+            $selected_author_id = -1;
+        }
 
-    // Set up post query arguments
-    $post_args = array (
-        'posts_per_page'    => -1, // Get all posts
-        'post_type'         => 'post',
-    );
+        // Set up post query arguments
+        $post_args = array (
+            'posts_per_page'    => -1, // Get all posts
+            'post_type'         => 'post',
+        );
 
-    // Modify query if a specific category is selected
-    if ( $selected_category_id != -1 ) {
-        $post_args [ 'category__in' ] = array ( $selected_category_id );
-    }
+        // Modify query if a specific category is selected
+        if ( $selected_category_id != -1 ) {
+            $post_args [ 'category__in' ] = array ( $selected_category_id );
+        }
 
-    // Modify query if a specific author is selected
-    if ( $selected_author_id != -1 ) {
-        $post_args [ 'author' ] = $selected_author_id;
-    }
+        // Modify query if a specific author is selected
+        if ( $selected_author_id != -1 ) {
+            $post_args [ 'author' ] = $selected_author_id;
+        }
 
-    // Execute the query
-    $query = new WP_Query( $post_args );
+        // Execute the query
+        $query = new WP_Query( $post_args );
 
-    // Get all authors
-    $authors = get_users( array ( 'role__in' => array ( 'author', 'administrator', 'editor', 'contributor' ) ) );
+        // Get all authors
+        $authors = get_users( array ( 'role__in' => array ( 'author', 'administrator', 'editor', 'contributor' ) ) );
 
-    // Get all categories
-    $categories = get_categories();
-   
-    require_once __DIR__ . '/templates/post-menu-markup.php';
+        // Get all categories
+        $categories = get_categories();
 
+        // Call the post-menu-markup file
+        require_once __DIR__ . '/templates/post-menu-markup.php';
     }
     
 }
--- post-management.php
+++ post-management.php
@@ -3,27 +3,28 @@
  * Plugin Name: Post Managesment Plugin
  * Description: This is the plugin for Partics purpose.
 */
-
 class post_managesment_plugin {
 
 	private static $instance;
 
 	public static function get_instance () {
+
 		if ( ! self::$instance ) {
 			self::$instance = new self();
 		}
-
 		return self::$instance;
-	}
 
+	}
 	private function __construct() {
-		$this->require_classes();
-	}
 
+		$this->require_classes();
+
+	}
 	private function require_classes() {
+
 		require_once __DIR__ . '/includes/admin-menu.php';
 		new Post_Management_Admin_Menu();
+		
 	}
 }
-
 post_managesment_plugin::get_instance();
